8b9d2141bb95d3ea8b49a8b01f34332ebb09e7e7
Rajasekharan Vengalil <rvengalil@microsoft.com>
2023-11-26 14:41:04 +0530

    Make service name an arg in sample app

--- samples/sf-connect/src/main.rs
+++ samples/sf-connect/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case, clippy::missing_safety_doc)]
 
+use std::env::args;
+
 use anyhow::Result;
 use sf_rs::{FabricLocalClient, PartitionKeyType, QueryClient, ServiceManagementClient};
 use windows::Win32::System::Com::{CoInitializeEx, COINIT_MULTITHREADED};
@@ -12,24 +14,25 @@ async fn main() -> Result<()> {
         CoInitializeEx(None, COINIT_MULTITHREADED)?;
     }
 
+    let args = args().collect::<Vec<_>>();
+    if args.len() < 2 {
+        eprintln!("Usage: sf-connect <service_name>");
+        return Ok(());
+    }
+
+    let service_name = args[1].clone();
+
     let client = FabricLocalClient::new()?;
     let service_client: ServiceManagementClient = client.make_client()?;
 
     let res = service_client
-        .resolve_service_partition(
-            "fabric:/ZZZ/BlockStorageSFPkg",
-            PartitionKeyType::Int64,
-            1,
-            1000,
-        )
+        .resolve_service_partition(&service_name, PartitionKeyType::Int64, 1, 1000)
         .await?;
 
     println!("{:#?}", res);
 
     let query_client: QueryClient = client.make_client()?;
-    let res = query_client
-        .get_partition_list("fabric:/ZZZ/BlockStorageSFPkg", 1000)
-        .await?;
+    let res = query_client.get_partition_list(&service_name, 1000).await?;
 
     println!("{:#?}", res);
 
